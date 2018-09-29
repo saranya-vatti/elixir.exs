7863f0131224767ed92f4f4eecb36d00cccb3051
saranya-vatti <saranya.vatti@gmail.com>
2018-09-29 13:20:17 -0400

    Added data parsing in tutorial

--- elhex_delivery/lib/postal_code/data_parser.ex
+++ elhex_delivery/lib/postal_code/data_parser.ex
@@ -5,5 +5,24 @@ defmodule ElhexDelivery.PostalCode.DataParser do
     [header | data_rows] = File.read!(@postal_codes_filepath) |> String.split("\n")
 
     data_rows
+    |> Enum.map(fn(row) ->
+      String.split(row, "\t")
+    end)
+    |> Enum.filter(fn(row) ->
+      case row do
+        [_postal_code, _, _, _, _, _latitude, _longitude] -> true
+        _ -> false
+      end
+    end)
+    |> Enum.map(fn(row) ->
+      [postal_code, _, _, _, _, latitude,longitude] = row
+      [postal_code, latitude,longitude]
+    end)
+    |> Enum.map(fn(row) ->
+      [postal_code, latitude,longitude] = row
+      latitude = latitude |> String.replace(" ", "") |> String.to_float
+      longitude = longitude |> String.replace(" ", "") |> String.to_float
+      {postal_code, {latitude, longitude}}
+    end)
   end
 end
