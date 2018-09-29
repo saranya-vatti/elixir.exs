8dc21897a155d62086f37bd5ed9b78a085960335
saranya-vatti <saranya.vatti@gmail.com>
2018-09-29 13:33:31 -0400

    efficient parsing method using stream in tutorial

--- elhex_delivery/lib/postal_code/data_parser.ex
+++ elhex_delivery/lib/postal_code/data_parser.ex
@@ -4,25 +4,35 @@ defmodule ElhexDelivery.PostalCode.DataParser do
   def parse_data do
     [header | data_rows] = File.read!(@postal_codes_filepath) |> String.split("\n")
 
+    # Using Stream instead of Enum decreased eunning tim from 3.7 seconds to 1.8 seconds
     data_rows
-    |> Enum.map(fn(row) ->
-      String.split(row, "\t")
-    end)
-    |> Enum.filter(fn(row) ->
-      case row do
-        [_postal_code, _, _, _, _, _latitude, _longitude] -> true
-        _ -> false
-      end
-    end)
-    |> Enum.map(fn(row) ->
-      [postal_code, _, _, _, _, latitude,longitude] = row
-      [postal_code, latitude,longitude]
-    end)
-    |> Enum.map(fn(row) ->
-      [postal_code, latitude,longitude] = row
-      latitude = latitude |> String.replace(" ", "") |> String.to_float
-      longitude = longitude |> String.replace(" ", "") |> String.to_float
-      {postal_code, {latitude, longitude}}
-    end)
+    |> Stream.map(&(String.split(&1, "\t")))
+    |> Stream.filter(&data_row?(&1))
+    |> Stream.map(&parse_data_columns(&1))
+    |> Stream.map(&format_row(&1))
+    |> Enum.into(%{})
+  end
+
+  defp data_row?(row) do
+    case row do
+      [_postal_code, _, _, _, _, _latitude, _longitude] -> true
+      _ -> false
+    end
+  end
+
+  defp parse_data_columns(row) do
+    [postal_code, _, _, _, _, latitude,longitude] = row
+    [postal_code, latitude,longitude]
+  end
+
+  defp parse_number(str) do
+    str |> String.replace(" ", "") |> String.to_float
+  end
+
+  # format three element list into a two element tuple
+  defp format_row([postal_code, latitude,longitude]) do
+    latitude = parse_number(latitude)
+    longitude = parse_number(longitude)
+    {postal_code, {latitude, longitude}}
   end
 end
