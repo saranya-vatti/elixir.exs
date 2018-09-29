47c4725217b5be218a19a7e427597782308afb7d
saranya-vatti <saranya.vatti@gmail.com>
2018-09-29 13:13:29 -0400

    Parsed data in tutorial application

---
+++ elhex_delivery/lib/postal_code/data_parser.ex
@@ -0,0 +1,9 @@
+defmodule ElhexDelivery.PostalCode.DataParser do
+  @postal_codes_filepath "data/2018_Gaz_zcta_national.txt"
+
+  def parse_data do
+    [header | data_rows] = File.read!(@postal_codes_filepath) |> String.split("\n")
+
+    data_rows
+  end
+end
